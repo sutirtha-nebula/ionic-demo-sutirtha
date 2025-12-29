dcb8569a08b622c900b9898f0d28f7166d0cb6cb
Sutirtha Dey <sutirtha@nebula.codes>
2025-12-29 12:39:18 +0530

    products added

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountMenuPage } from '../components/account-menu/account-menu.page';
 import { PopoverController } from '@ionic/angular';
+import { ApiService } from '../services/api';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,8 +10,9 @@ import { PopoverController } from '@ionic/angular';
   standalone: false
 })
 export class DashboardPage {
+  products: any[] = [];
 
-  constructor(private popoverCtrl: PopoverController) { }
+  constructor(private popoverCtrl: PopoverController, private apiService: ApiService) { }
 
   async openAccountMenu(ev: any) {
     const popover = await this.popoverCtrl.create({
@@ -22,6 +24,14 @@ export class DashboardPage {
     });
     
     await popover.present();
-}
+  }
+
+  async ngOnInit() {
+    this.apiService.get('products').subscribe(data => {
+      this.products = data?.products || [];
+      console.log(this.products);
+      console.log('Products Data:', data);
+    });
+  }
 
 }
